8d3d2394fa665c057998ae72e159c71d744a268c
sminha <sminha8590@naver.com>
2023-05-02 17:03:19 +0900

    feat: 회원가입 기능 추가

--- ossp/login/urls.py
+++ ossp/login/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('main/', views.mainpage),
     path('auth/', auth_views.LoginView.as_view(template_name = 'login/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='login/newvbar.html'), name='logout'),
-    # 수정해야하는 코드
-    # django.contrib.auth앱의 LoginView 클래스를 활용했으므로 별도의 views.py 파일 수정이 필요 없음
+    path('signup/', views.signup, name='signup'),
 ]

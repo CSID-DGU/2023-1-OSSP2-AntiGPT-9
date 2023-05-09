1a475504ecbb558ff6e6fcf510a7a8a3389cdb3c
dgusmk <wwir9898@gmail.com>
2023-05-09 10:50:57 +0900

    fix: 경로 오류 수정 불필요 파일 제거

--- ossp/settings.py
+++ ossp/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls',
+    'home',
     'login',
+    'initpage',
 ]
 
 MIDDLEWARE = [

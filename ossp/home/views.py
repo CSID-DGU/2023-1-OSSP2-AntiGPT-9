f0d17ff26cf98312f9bffe980559d2c3948cad94
Jhsysng <screenshot161@gmail.com>
2023-05-30 11:11:02 +0900

    refactor: ip변경된 api적용

--- ossp/home/views.py
+++ ossp/home/views.py
@@ -4,16 +4,19 @@ from .models import *
 import requests
 import json
 
+
 def home(request):
     chatList = ChatRoom.objects.order_by('last_date')
     context = {'chatList':chatList}
     return render(request, 'home/homepage.html', context)
+
 
 def chat(request, chatroom_id):
     chatList = ChatRoom.objects.order_by('last_date')
     sets = ChatRoom.objects.get(id = chatroom_id).chatset_set.all()
     context = {'chatList':chatList, 'sets':sets, 'chat_id':chatroom_id}
     return render(request, 'home/chatpage.html', context)
+
 
 def question_send(request, chatroom_id):
     room = get_object_or_404(ChatRoom, pk=chatroom_id)
@@ -29,7 +32,7 @@ def api_commute(dialect):
     request_dialect = json.dumps(dialect_dict)
 
     # standard transfer, jeju model api request
-    standard = requests.post("http://34.83.150.5:8010/jeju", data=request_dialect)
+    standard = requests.post("http://4.194.73.164:8010/jeju", data=request_dialect)
 
     # transfer json to dict, return standard string
     standard = json.loads(standard)
